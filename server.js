485ac08477cbcdb572f6909f8c3e804b8ac94985
Anton Stepan <info@antonstepan.it>
2016-05-01 22:52:34 +0200

    ...

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const config = configBuilder(DEV_ENV);
 
 const compiler = webpack(config);
 
-if (env === DEV_ENV) {
+//if (env === DEV_ENV) {
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
   }));
   app.use(webpackHotMiddleware(compiler));
-}
+//}
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/src/index.html');
