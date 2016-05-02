425e29c1cbbaa1508a08c5edd738d7e6f5007cb8
Anton Stepan <info@antonstepan.it>
2016-05-02 19:12:04 +0200

    fu

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,8 @@ module.exports = {
     //   }
     // }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.BABEL_ENV': JSON.stringify('production')
     })
   ],
   context: path.resolve(__dirname, ''),
