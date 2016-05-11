82287964273ce9474fb483138671b4797cee0739
Anton Stepan <info@antonstepan.it>
2016-05-11 19:03:41 +0200

    added styles and themes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import Root from './root'
 
+import './styles/_global.scss'
+
 render(<Root />, document.getElementById('root'))
---
+++ src/themes/colors.js
@@ -0,0 +1,25 @@
+
+const colors = {
+  // https://github.com/infinitered/ignite/blob/master/ignite-base/App/Themes/Colors.js
+  rainbow: 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')',
+  background: 'white',
+  snow: 'white',
+  clear: 'rgba(0,0,0,0)',
+  ocean: '#415572',
+  deepOcean: '#32435c',
+  wave: '#0083b0',
+  facebook: '#3b5998',
+  transparent: 'rgba(0,0,0,0)',
+  silver: '#F7F7F7',
+  steel: '#CCCCCC',
+  error: 'rgba(200, 0, 0, 0.8)',
+  ricePaper: 'rgba(255,255,255, 0.75)',
+  windowTint: 'rgba(0, 0, 0, 0.4)',
+  panther: '#464646',
+  charcoal: '#595959',
+  coal: '#2d2d2d',
+  bloodOrange: '#fb5f26',
+  bloodOrangeJuice: 'rgba(251, 95, 38, 0.8)'
+}
+
+export default colors
